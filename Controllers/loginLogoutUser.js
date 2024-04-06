83b94bf84811bd32bc0a1b99b50012321466f530
adamokennie <adamokennie@gmail.com>
2024-04-06 13:46:34 +0100

    register and login functionality with server setup

---
+++ Controllers/loginLogoutUser.js
@@ -0,0 +1,69 @@
+import User from "../models/userModel.js";
+import asynchandler from "express-async-handler";
+import bcrypt from "bcryptjs";
+import genToken from "./tokenGen.js";
+
+
+const loginUser = asynchandler( async(req, res) => {
+    const { email, password } = req.body;
+
+    //validate required fields
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("Please provide correct email and password");
+    }
+    
+    //find user by email
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        res.status(400);
+        throw new Error("User not found, Please sign up!");
+    }
+    
+    //check password
+    const passwordIsValid = await bcrypt.compare(password, user.password);
+
+    // generate token
+    const token = genToken(user._id);
+
+    if (passwordIsValid) {
+        //send cookie to server
+        res.cookie("token", token, {
+            path: "/",
+            httpOnly: true,
+            expires: new Date(Date.now() + 1000 * 24 * 60 * 60),
+            sameSite:"none",
+            secure:true,
+        });
+    }
+    if (user && passwordIsValid){
+        const { _id, name, email, password, photo, bio } = user;
+        res.status(200).json({
+            _id,
+            name,
+            email,
+            password,
+            photo,
+            bio,
+            token,
+    });
+    }else{
+        res.status(400);
+        throw new Error("Invalid Email or password");
+    }
+});
+
+const logOut = asynchandler( async (req, res) => {
+    // expire the session
+    res.cookie("token", "", {
+        path: "/",
+        httpOnly: true,
+        expires: new Date,
+        sameSite:"none",
+        secure:true
+    });
+    return res.status(200).json({ message: "Sucessfully logged out"});
+});
+
+export  { loginUser, logOut };
--- server.js
+++ server.js
@@ -1,1 +1,38 @@
-const express=require("express")
+import cors from  "cors";
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import connectDB  from "./Config/db.js";
+// import errorHandler from "./middleware/errorMiddleware.js";
+import cookieParser from "cookie-parser";
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT;
+
+//connect to db
+connectDB();
+
+//app middleware
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// routes middleware
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+
+
+//route
+app.get("/", (req, res) => {
+    res.send("Home Page!");
+});
+
+//error handler
+app.use(errorHandler);
+//start server
+    app.listen(PORT, () => {
+        console.log(`Server is running on ${PORT}`);
+    });
