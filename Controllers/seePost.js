c9bfda477526887cbfaac0193726f5ed12daae73
Adamo  kehinde hassan <102253825+lambojac@users.noreply.github.com>
2024-09-18 20:28:13 +0100

    Update seePost.js

--- Controllers/seePost.js
+++ Controllers/seePost.js
@@ -16,7 +16,7 @@ const seePost=async (req, res) => {
         res.status(200).json({ feed: followingPosts });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: "Server error" });
+        res.status(400).json({ message: "Server error" });
     }
 }
 export default seePost
